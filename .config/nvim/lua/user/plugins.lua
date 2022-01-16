0aaae64ebc17a6dde5b0d5a627fee23e16124df8
Jesse <username.git@posteo.net>
2022-01-16 00:30:33 -0800

    update dotfiles

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -51,6 +51,7 @@ return packer.startup(function(use)
 	use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
 	use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 
+	use 'navarasu/onedark.nvim'
 	use "sainnhe/gruvbox-material"
 	use "windwp/nvim-autopairs"
 	use "lewis6991/gitsigns.nvim"
