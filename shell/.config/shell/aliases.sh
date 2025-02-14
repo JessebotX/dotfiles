955386cdd5dab1564724123c0cda024522862de9
Jesse <jessebot.git@gmail.com>
2025-02-13 22:15:37 -0800

    feat(shell): add alias for terminal searching/lookup

--- shell/.config/shell/aliases.sh
+++ shell/.config/shell/aliases.sh
@@ -2,6 +2,10 @@
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
+### lynx
+
+alias ?="search"
+
 ### ls
 
 if [ -x /usr/bin/dircolors ]; then
