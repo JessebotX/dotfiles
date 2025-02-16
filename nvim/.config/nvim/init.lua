845b20c82025280dc456fdeeb4edbe7b878f5ec5
Jesse <jessebot.git@gmail.com>
2025-02-15 19:58:42 -0800

    refactor(nvim): set golang tabstop to 8

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -104,7 +104,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.api.nvim_create_autocmd('FileType', {
     pattern = 'go',
     callback = function()
-        vim.opt_local.tabstop = 4
+        vim.opt_local.tabstop = 8
         vim.opt_local.expandtab = false
     end,
 })
