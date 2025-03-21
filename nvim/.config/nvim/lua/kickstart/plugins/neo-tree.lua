1e6cba26581076937562e08b55fc04a44a89fcfd
Jesse <jessebot.git@gmail.com>
2025-03-20 22:26:40 -0700

    feat(nvim): change neotree default configuration

--- nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -2,24 +2,25 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+        'MunifTanjim/nui.nvim',
     },
-  },
+    cmd = 'Neotree',
+    keys = {
+        { '\\', ':Neotree reveal_force_cwd reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    },
+    opts = {
+        close_if_last_window = true,
+        filesystem = {
+            window = {
+                mappings = {
+                    ['\\'] = 'close_window',
+                },
+            },
+        },
+    },
 }
